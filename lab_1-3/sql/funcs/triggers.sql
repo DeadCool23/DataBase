15e696d954b0cb6f84953377228b55b03459752d
nisuev <nisuev04@mail.ru>
2024-10-21 16:01:49 +0300

    fix lab_03

--- lab_1-3/sql/funcs/triggers.sql
+++ lab_1-3/sql/funcs/triggers.sql
@@ -15,17 +15,20 @@ EXECUTE FUNCTION update_request_status_after_repair();
 
 -- ==================================================================================
 
-CREATE OR REPLACE FUNCTION set_request_date()
+CREATE OR REPLACE FUNCTION log_requeste_date()
 RETURNS TRIGGER AS $$
 BEGIN
-    NEW.request_date := CURRENT_DATE;
+    RAISE NOTICE 'New request: [id = %, status = %, date = %]', NEW.id, NEW.request_status, CURRENT_DATE;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER before_request_insert
-BEFORE INSERT ON requests
-FOR EACH ROW
-EXECUTE FUNCTION set_request_date();
+CREATE VIEW req_log AS
+SELECT id, request_status, request_date
+FROM requests;
 
+CREATE TRIGGER before_request_insert
+INSTEAD OF INSERT ON req_log
+FOR EACH ROW
+EXECUTE FUNCTION log_requeste_date();
